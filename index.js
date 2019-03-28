954f0ef8632a4f81aa999ddfd9a7d17913f93d18
Gregori Kanatzidis <kanatzidisg@gmail.com>
2019-03-27 19:38:45 -0500

    v0.1.0

---
+++ index.js
@@ -0,0 +1,86 @@
+const https = require('https');
+const http = require('http');
+const qs = require('querystring');
+
+const API = {};
+
+async function kvs({database, key, value, del}, options={}) {
+  if(database !== undefined && typeof database !== 'string') database = database.toString();
+  if(key !== undefined && typeof key !== 'string') key = key.toString();
+  if(value !== undefined && typeof value !== 'string') {
+    if(typeof value === 'object') value = JSON.stringify(value);
+    else value = value.toString();
+  }
+  if(database && database[0] === '`') throw new Error('Database names cannot start with `');
+  if(key && key[0] === '`') throw new Error('Keys cannot start with `');
+
+  var method = del ? 'DELETE'
+    : (value === undefined
+      ? 'GET' : 'PUT')
+
+  var query = '';
+
+  if(method === 'GET' && !key && options.keys) {
+    query = options.keys === true
+      ? '?keys'
+      : `?keys=${options.keys.map(k => qs.escape(k)).join(',')}`
+  }
+
+  return request({
+    hostname: API.kvs.host,
+    port: API.kvs.port,
+    headers: API.kvs.token ? { 'X-Api-Key': API.kvs.token } : {},
+    path: API.kvs.prefix + (database ? `/${encodeURIComponent(database)}` : '') + (key ? `/${encodeURIComponent(key)}` : '') + query,
+    body: value,
+    method
+  });
+}
+
+async function request(options) {
+
+  return new Promise((resolve, reject) => {
+    var request = options.port == 443
+      ? https.request(Object.assign(options, {port: 443}), handleResponse)
+      : http.request(options, handleResponse);
+    function handleResponse(response) {
+      var data = '';
+
+      response.on('error', reject);
+
+      response.on('data', d => data += d);
+
+      response.on('end', () => resolve(response.headers['content-type'] === 'application/json' ? JSON.parse(data) : data));
+    };
+
+    request.on('error', reject);
+
+    request.end(options.body);
+  });
+}
+
+API.kvs = {
+  host: 'kvs.nyan.sh',
+  port: '443',
+  prefix: '',
+  listDbs: async () => {
+    return kvs({});
+  },
+  list: async (database, {keys}={}) => {
+    return kvs({database}, {keys});
+  },
+  get: async (database, key) => {
+    return kvs({database,key});
+  },
+  set: async (database, key, data) => {
+    return kvs({database,key,value:data});
+  },
+  delete: async (database, key) => {
+    return kvs({database,key,del:true});
+  },
+  deleteDb: async(database) => {
+    return kvs({database,del:true});
+  }
+};
+
+
+module.exports = API;
---
+++ test.js
@@ -0,0 +1,63 @@
+const {kvs} = require('./');
+const assert = require('assert');
+
+kvs.host = 'localhost';
+kvs.port = 1338;
+kvs.token = process.env.KVS_TOKEN;
+
+async function run() {
+
+  // clear test dbs
+  await kvs.deleteDb('kvs_tests');
+  await kvs.deleteDb('kvs_tests2');
+  assert.equal('{}', await kvs.list('kvs_tests'));
+  assert.equal('{}', await kvs.list('kvs_tests2'));
+
+  // define test k-v pairs
+  const key1 = `als;lae;rlgajwe;afiwejflaSCefa'fawef;awefawefjew;f,W;WO/3//3 ?LA#(#R#O`
+  const value1 = 'a less annoying value';
+  const key2 = 'a sane key';
+  const value2 = '1032u0qfjeafS#OP{"3q;';
+  const key3 = 'users.myusername.token';
+  const value3 = kvs.token;
+
+  // set, list, get
+  await kvs.set('kvs_tests', key1, value1);
+  assert.equal(JSON.stringify({[key1]:value1}), await kvs.list('kvs_tests'));
+  assert.equal('{}', await kvs.list('kvs_tests2'));
+  await kvs.set('kvs_tests2', value1, key1);
+  assert.equal(key1, await kvs.get('kvs_tests2', value1));
+
+  // list dbs
+  assert.deepEqual(['kvs_tests', 'kvs_tests2'], (await kvs.listDbs()).split('\n').sort());
+
+  // set, delete
+  await kvs.set('kvs_tests2', key2, value2);
+  await kvs.set('kvs_tests2', key3, value3);
+  assert.deepEqual(
+    {
+      [value1]:key1,
+      [key2]:value2,
+      [key3]:value3
+    },
+    JSON.parse(await kvs.list('kvs_tests2'))
+  );
+  await kvs.delete('kvs_tests2', key2);
+  assert.deepEqual(
+    {
+      [value1]:key1,
+      [key3]:value3
+    },
+    JSON.parse(await kvs.list('kvs_tests2'))
+  );
+
+  // delete db
+  await kvs.deleteDb('kvs_tests');
+  await kvs.deleteDb('kvs_tests2');
+  assert.equal('{}', await kvs.list('kvs_tests'));
+  assert.equal('{}', await kvs.list('kvs_tests2'));
+
+  console.log('tests were successful');
+}
+
+run();
